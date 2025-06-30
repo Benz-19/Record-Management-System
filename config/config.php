aeeb67e71c272ae84e32419aa058df0d3638f9c7
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-06-30 16:04:33 +0300

    update

--- config/config.php
+++ config/config.php
@@ -1,7 +1,10 @@
 <?php
 
-require_once __DIR__ .'/../bootstrap.php';
-
-return[
-
-]
+return [
+    'db_name' => $_ENV['DB_NAME'],
+    'db_host' => $_ENV['DB_HOST'],
+    'db_username' => $_ENV['DB_USERNAME'],
+    'db_password' => $_ENV['DB_PASSWORD'],
+    'port' => '3306',
+    'charset' => 'utf8mb4'
+];
