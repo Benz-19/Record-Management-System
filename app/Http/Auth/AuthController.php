7a04b48306aa5c6a53edb21bfd0dba73f24307f0
ugwu_kingsley <kingsleyikenna2019@gmail.com>
2025-07-02 11:50:23 +0300

    new features

--- app/Http/Auth/AuthController.php
+++ app/Http/Auth/AuthController.php
@@ -8,6 +8,13 @@ use App\Models\User;
 class AuthController
 {
 
+    /**
+     * Login processes the login functionality of any user (admin, client)
+     * @param string $_SESSION['error'] stores potential error to display to
+     * the user but would be unset after use
+     * @param array $is_user stores the user data obtained form the db such as. username,user_type etc.
+     */
+
     public static function login()
     {
         if (!isset($_POST['loginBtn'])) {
@@ -22,26 +29,26 @@ class AuthController
             exit;
         } else {
             // Sanitize input
-            $input_email = htmlspecialchars(trim($_POST['email']));
+            $input_email = strtolower(htmlspecialchars(trim($_POST['email'])));
             $input_password = htmlspecialchars(trim($_POST['password']));
 
             // Validate if this is a user
-            $is_user =  User::isUser((string)$input_email, (string)$input_password);
+            $is_user =  (new User)->isUser((string)$input_email, (string)$input_password);
 
             if (empty($is_user) || $is_user === null) {
                 $_SESSION['error'] = 'User does not exists';
-                header('Location: /record_management_system/');
+                header('Location: /record_management_system/login'); //redirect to login page if not a user
                 exit;
             } else {
                 if ($is_user['user_type'] === 'admin') {
-                    header('Location: /record_management_system/admin/dashboard');
+                    header('Location: /record_management_system/admin/dashboard'); //redirects to admin dashboard
                     exit;
                 } elseif ($is_user['user_type'] === 'client') {
-                    header('Location: /record_management_system/client/dashboard');
+                    header('Location: /record_management_system/client/dashboard'); //redirects to client dashboard
                     exit;
                 } else {
                     $_SESSION['error'] = 'Login process failed';
-                    header('Location: /record_management_system/login');
+                    header('Location: /record_management_system/login'); //redirect to login page on failed attempt
                     exit;
                 }
             }
@@ -50,6 +57,17 @@ class AuthController
 
     public static function register()
     {
-        //
+        if (!isset($_POST['registerBtn'])) {
+            $_SESSION['error'] = 'Something went wrong...';
+            header('Location: /record_management_system/login');
+            exit;
+        }
+
+        if (!isset($_POST['username']) || !isset($_POST['email']) || !isset($_POST['password'])) {
+            $_SESSION['error'] = 'Ensure all fields are filled...';
+            header('Location: /record_management_system/login');
+            exit;
+        } else {
+        }
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -14,22 +14,44 @@ class User
         $this->db = new DB();
     }
 
-    public static function isUser(string $email, string $password)
+    public function isUser(string $email, string $password)
     {
         $db = new DB();
 
         try {
-            $query = "SELECT * FROM users WHERE email=:email AND password=:pass";
-            $params = [
-                ':email' => $email,
-                ':pass' => $password
-            ];
-            $result = $db->execute($query, $params);
+            $user_credentials = self::getUserCredentials($email);
 
-            return $result ?: null; //returns the user data
+            if (empty($user_credentials) || $user_credentials === null) {
+                $_SESSION['error'] = 'User does not exists!!!';
+                header('Location: /record_management_system/login');
+                exit;
+            } else {
+                if (password_verify($password, $user_credentials['password'])) {
+                    return $user_credentials;  //return the entire array for validation at login
+                }
 
+                return null;
+            }
         } catch (PDOException $error) {
             error_log('Failed to validate user. ErrorType: ' . $error->getMessage());
+            return null;
+        }
+        return null;
+    }
+
+    public function getUserCredentials(string $email)
+    {
+        $db = new DB();
+
+        try {
+            $query = "SELECT * FROM users WHERE email=:email";
+            $params = [':email' => strtolower(htmlspecialchars(trim($email)))];
+
+            $user_credentials = $db->fetchSingleData($query, $params);
+
+            return $user_credentials ?: null; //returns the user data
+        } catch (PDOException $error) {
+            error_log('Failed to get user credentials user. ErrorType: ' . $error->getMessage());
             return null;
         }
 
